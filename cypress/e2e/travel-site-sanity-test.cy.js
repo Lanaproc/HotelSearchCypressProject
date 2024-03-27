9ece2484fe61ba5d07c852c786d287069533affe
Sveta Protsenko <svitlanaprotsenko@gmail.com>
2024-03-27 13:33:25 -0400

    Changes to be committed: 	new file:   cypress/e2e/cypress.config.js 	new file:   cypress/e2e/cypress/fixtures/example.json 	new file:   cypress/e2e/cypress/support/commands.js 	new file:   cypress/e2e/cypress/support/e2e.js 	modified:   cypress/e2e/travel-site-sanity-test.cy.js

--- cypress/e2e/travel-site-sanity-test.cy.js
+++ cypress/e2e/travel-site-sanity-test.cy.js
@@ -22,14 +22,14 @@ describe('The sanity test suite for travel site', () => {
     cy.get('[data-cy="Flight"]').should("have.text", "flight")
   })
 
-  it('verify elements of Hotel tab', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('[data-cy="flight_chk"]').should("be.visible")
-    cy.get('[data-cy="cars_chk"]').should("be.visible")
-   // cy.get('#tab1 > form > .btn-submit').should("be.visible")
-   //cy.contains('search').should("be.visible")
-   cy.get('[type="submit"]').should("be.visible")
-  })
+  // it('verify elements of Hotel tab', () => {
+  //   cy.visit('http://localhost:3000')
+  //   cy.get('[data-cy="flight_chk"]').should("be.visible")
+  //   cy.get('[data-cy="cars_chk"]').should("be.visible")
+  //  // cy.get('#tab1 > form > .btn-submit').should("be.visible")
+  //  //cy.contains('search').should("be.visible")
+  //  cy.get('[type="submit"]').should("be.visible")
+  // })
 
 
 })
